08839d27eb68c5dcef8ede47c97504d91ae8b921
Jordan Dukart <jordan@discoverygarden.ca>
2013-01-17 15:17:21 -0400

    Push changes from local.

---
+++ templates/islandora-pdf.tpl.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @file
+ * This is the template file for the pdf object
+ *
+ * @TODO: Add documentation about this file and the available variables
+ * @TODO: don't call drupal_set_title here
+ */
+?>
+
+<div class="islandora-pdf-object islandora">
+  <div class="islandora-pdf-content-wrapper clearfix">
+    <?php if (isset($islandora_content)): ?>
+      <div class="islandora-pdf-content">
+        <?php print $islandora_content; ?>
+      </div>
+      <?php print $islandora_download_link; ?>
+    <?php endif; ?>
+  <div class="islandora-pdf-sidebar">
+    <?php if (!empty($dc_array['dc:description']['value'])): ?>
+      <h2><?php print $dc_array['dc:description']['label']; ?></h2>
+      <p><?php print $dc_array['dc:description']['value']; ?></p>
+    <?php endif; ?>
+    <?php if($parent_collections): ?>
+      <div>
+        <h2><?php print t('In collections'); ?></h2>
+        <ul>
+          <?php foreach ($parent_collections as $collection): ?>
+            <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+    <?php endif; ?>
+  </div>
+  </div>
+</div>
